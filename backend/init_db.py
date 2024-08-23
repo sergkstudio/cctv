ac34ee34837f5cd163950c1bd88e10bcf66a0f84
sergkstudio <sergkstudio@gmail.com>
2024-08-23 11:08:48 +0300

    add admin

---
+++ backend/init_db.py
@@ -0,0 +1,17 @@
+# backend/init_db.py
+from app import app, db
+from models import User
+
+def create_admin():
+    with app.app_context():
+        admin = User.query.filter_by(username='admin').first()
+        if admin is None:
+            admin = User(username='admin')
+            admin.set_password('Kolobok201@')  # Задайте безопасный пароль
+            admin.is_admin = True
+            db.session.add(admin)
+            db.session.commit()
+            print("Admin user created successfully.")
+
+if __name__ == '__main__':
+    create_admin()
