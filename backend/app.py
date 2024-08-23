b7067c9cb6e1d85f161bdf3ba32919d2d01c57dc
sergkstudio <sergkstudio@gmail.com>
2024-08-23 13:16:58 +0300

    123

--- backend/app.py
+++ backend/app.py
@@ -1,49 +1,9 @@
-# backend/app.py
-from flask import Flask, request, jsonify
-from models import db, User
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db:5432/mydatabase'
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://user:password@db:5432/mydatabase')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
-jwt = JWTManager(app)
-
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-    
-    if User.query.filter_by(username=username).first():
-        return jsonify({'message': 'User already exists'}), 400
-
-    user = User(username=username)
-    user.set_password(password)
-    db.session.add(user)
-    db.session.commit()
-    
-    return jsonify({'message': 'User registered successfully'}), 201
-
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-
-    user = User.query.filter_by(username=username).first()
-    if user and user.check_password(password):
-        access_token = create_access_token(identity={'username': username})
-        return jsonify(access_token=access_token)
-    else:
-        return jsonify({'message': 'Invalid credentials'}), 401
-
-@app.route('/protected', methods=['GET'])
-@jwt_required()
-def protected():
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+db = SQLAlchemy(app)
